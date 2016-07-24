5bff14b58465f4694af0547aae62df03bd897eef
ngld <ngld@tproxy.de>
2016-07-24 22:31:36 +0200

    Implemented uploads

--- slib/controllers.py
+++ slib/controllers.py
@@ -13,6 +13,11 @@
 ## limitations under the License.
 
 import logging
+import time
+import hashlib
+import os.path
+import tempfile
+import shutil
 
 from flask import request, json, jsonify, render_template
 
@@ -34,6 +39,86 @@ def task_monitor():
 @app.route('/watch/<int:task_id>')
 def render_watcher(task_id):
     return render_template('watcher.html', task_id=task_id)
+
+
+@app.route('/drop/<int:t>/<token>/<int:kid>', methods=('POST', 'OPTIONS'))
+def receive_drop(t, token, kid):
+    if app.config.get('UPLOAD_PATH', None) is None or app.config.get('MIRROR_PATH', None) is None:
+        return 'Access denied', 403
+
+    headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'POST, OPTIONS',
+        'Access-Control-Max-Age': '86400'
+    }
+
+    if request.method == 'OPTIONS':
+        return '', 200, headers
+
+    if time.time() > t:
+        return 'Access denied', 403, headers
+
+    try:
+        key = app.config['API_KEYS'][kid]
+    except KeyError:
+        return 'Access denied', 403, headers
+
+    my_tk = hashlib.new('sha256')
+    my_tk.update(('%s%d' % (key, t)).encode('utf8'))
+    my_tk = my_tk.hexdigest()
+    if my_tk != token:
+        return 'Access denied', 403, headers
+
+    if 'file' not in request.files:
+        return 'Access denied', 403, headers
+
+    file = request.files['file']
+    if file.filename == '' or '.' not in file.filename or file.filename.rsplit('.', 1)[1] not in app.config['UPLOAD_EXTENSIONS']:
+        return jsonify(
+            error=True,
+            message='Invalid filename!'
+        ), 200, headers
+
+    fhash = hashlib.new('sha256')
+    fs = file.stream
+    buf = None
+
+    if not file.stream.seekable():
+        buf = tempfile.TemporaryFile()
+
+        # This while loop is copy&pasted from below to avoid checking buf on every iteration
+        while True:
+            chunk = fs.read(32 * fhash.block_size)
+            if not chunk:
+                break
+
+            fhash.update(chunk)
+            buf.write(chunk)
+    else:
+        while True:
+            chunk = fs.read(32 * fhash.block_size)
+            if not chunk:
+                break
+
+            fhash.update(chunk)
+
+    suffix = app.config['UPLOAD_PATH'] + '/' + fhash.hexdigest()
+    dst = os.path.join(app.config['MIRROR_PATH'], suffix)
+    
+    if not os.path.isfile(dst):
+        if buf is None:
+            file.stream.seek(0)
+            file.save(dst)
+        else:
+            with open(dst, 'wb') as stream:
+                shutil.copyfileobj(buf, stream)
+
+            buf.close()
+
+    return jsonify(
+        error=False,
+        url=app.config['MIRROR_URL'] + '/' + suffix
+    ), 200, headers
 
 
 @app.route('/api/converter/request', methods=('POST',))
